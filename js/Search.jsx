b635497a6b5f6a518cd1f4ec45e136e401b16b6a
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2017-09-17 03:23:00 -0500

    adds styled components

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,7 +4,9 @@ import ShowCard from "./ShowCard";
 
 const Search = () => (
   <div className="search">
-    {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+    <div>
+      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+    </div>
   </div>
 );
 
--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { string } from "prop-types";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 40%;
+  float: left;
+  margin-right: 10px;
+`;
 
 const ShowCard = props => (
-  <div className="show-card">
-    <img
+  <Wrapper>
+    <Image
       src={`/public/img/posters/${props.poster}`}
       alt={`${props.title} Show Poster`}
     />
@@ -12,7 +28,7 @@ const ShowCard = props => (
       <h4>({props.year})</h4>
       <p>{props.description}</p>
     </div>
-  </div>
+  </Wrapper>
 );
 
 ShowCard.propTypes = {
