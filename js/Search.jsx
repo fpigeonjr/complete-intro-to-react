a6c1edff122edd7dfbef618d683635a6775b0fe2
Frank Pigeon <frank.pigeonjr@gmail.com>
2017-09-17 15:59:19 -0500

    refactors to React Class to allow for state

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
-
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "this is some sort of debug statement"
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 export default Search;
