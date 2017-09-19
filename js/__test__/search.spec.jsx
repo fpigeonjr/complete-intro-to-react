8d669b26439af9b764ae8d41443124c06bd1ddc5
Frank Pigeon <frank.pigeonjr@gmail.com>
2017-09-19 05:31:10 -0500

    adds enzyme and search test

--- js/__test__/search.spec.jsx
+++ js/__test__/search.spec.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+import preload from "../../data.json";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
 
 test("Search renders correctly", () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
 });
+
+test("Search should render correct amount of shows", () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+xtest("Search should render correct number of terms based on ");
