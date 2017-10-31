83a1617b171021e6ecc5e7042ffc40cfd3119d64
Frank Pigeon <frank.pigeonjr@gmail.com>
2017-10-31 00:04:18 -0500

    adds Detail component. Updates prettier.

--- js/__test__/search.spec.jsx
+++ js/__test__/search.spec.jsx
@@ -5,18 +5,18 @@ import Search from "../Search";
 import ShowCard from "../ShowCard";
 
 test("Search renders correctly", () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows}/>);
   expect(component).toMatchSnapshot();
 });
 
 test("Search should render correct amount of shows", () => {
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows}/>);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test("Search should render correct amount of shows based on search", () => {
   const searchWord = "house";
-  const component = shallow(<Search />);
+  const component = shallow(<Search shows={preload.shows} />);
   component.find("input").simulate("change", { target: { value: searchWord } });
   const showCount = preload.shows.filter(show =>
     `${show.title.toUpperCase()} ${show.description.toUpperCase()}`.includes(
